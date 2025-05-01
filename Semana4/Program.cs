6ac97d70a068c0d8e72c746e00750788f2a2fec8
Osvaldo <osvaldoguerra9909@gmail.com>
2025-05-01 17:16:47 -0500

    Ejercicios Resueltos

--- Semana4/Program.cs
+++ Semana4/Program.cs
@@ -10,7 +10,7 @@ namespace Semana4
     {
         static void Main(string[] args)
         {
-            ejercicio3();
+            ejercicio6();
             Console.ReadKey();
         }
 
@@ -56,22 +56,88 @@ namespace Semana4
 
         static void ejercicio3()
         {
+            Console.Write("Ingrese sueldo base: ");
+            double sueldo = double.Parse(Console.ReadLine());
 
+            double maria = (sueldo * 0.95) - 25;
+            double juan = (sueldo * 1.23) + 72;
+            double patricio = (sueldo * 1.05) - 56;
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Sueldo de María: {maria:F2}");
+            Console.WriteLine($"Sueldo de Juan: {juan:F2}");
+            Console.WriteLine($"Sueldo de Patricio: {patricio:F2}");
+            Console.WriteLine($"Suma de sueldos: {maria+juan+patricio:F2}");
         }
 
-        static void ejrcicio4()
+        static void ejercicio4()
         {
-
+            Console.Write("Ingrese un año: ");
+            int an = int.Parse(Console.ReadLine());
+            if (an%4==0 & an%100!=0 | (an%400==0))
+            {
+                Console.WriteLine("Es año bisiesto");
+            }
+            else
+                Console.WriteLine("No es año bisiesto");   
         }
 
         static void ejercicio5()
         {
+            Console.Write("Ingrese un monto en S/: ");
+            double monto = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Seleccione moneda: \n1. Dolares \n2. Euros");
+            Console.WriteLine("Ingrse una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1: Console.WriteLine($"USD: {(monto / 3.75):F2}");
+                    break;
+                case 2: Console.WriteLine($"EUR: {(monto / 4.05):F2}");
+                    break;
+                default: Console.WriteLine("Opción incorrecta");
+                    break;
+            }
         }
 
         static void ejercicio6()
         {
+            Console.WriteLine("BIENVENIDO AL SISTEMA DE CALCULO DE ÁREAS");
+            Console.WriteLine("\n1. Cuadrado \n2. Rectángulo \n3. Triángulo \n4. Círculo");
+            Console.Write("\nIngrese la opción: ");
+            int opcion = int.Parse(Console.ReadLine());
 
+            Console.WriteLine();
+
+            switch (opcion)
+            {
+                case 1: Console.Write("Ingrese el lado: ");
+                    int lado = int.Parse(Console.ReadLine());
+                    Console.Write("El área del cuadrado es: " + (lado * lado));
+                    break;
+                case 2: Console.Write("Ingrese la base: ");
+                    int baseR = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura: ");
+                    int altR = int.Parse(Console.ReadLine());
+                    Console.Write("El área del rectángulo es: " + (baseR * altR));
+                    break;
+                case 3:
+                    Console.Write("Ingrese la base: ");
+                    int baseT = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura: ");
+                    int altT = int.Parse(Console.ReadLine());
+                    Console.Write("El área del triángulo es: " + (baseT * altT) / 2);
+                    break;
+                case 4: Console.Write("Ingrese el radio: ");
+                    double radio = double.Parse(Console.ReadLine());
+                    Console.Write($"El área del círculo es: {(Math.PI * Math.Pow(radio,2)):F2}");
+                    break;
+                default : Console.Write("Opción no valida");
+                    break;                 
+            }
         }
     }
 }
