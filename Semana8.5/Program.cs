2e4109263c34943f504467952698a2df93f92c07
Osvaldo <osvaldoguerra9909@gmail.com>
2025-05-29 02:21:52 -0500

    Ejercicios Resuletos - S8

--- Semana8.5/Program.cs
+++ Semana8.5/Program.cs
@@ -13,42 +13,58 @@ namespace Semana8._5
             int opcion;
             char continuar;
 
-            do {
-                Console.WriteLine("1. Suma");
-                Console.WriteLine("2. Resta");
-                Console.WriteLine("3. Multiplicación");
-                Console.WriteLine("4. División");
-                Console.WriteLine("0. Salir");
+            do
+            {
 
-                Console.Write("\nIngrese una opcion: ");
-                opcion = int.Parse(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("1. Suma");
+                    Console.WriteLine("2. Resta");
+                    Console.WriteLine("3. Multiplicación");
+                    Console.WriteLine("4. División");
+                    Console.WriteLine("0. Salir");
+
+                    Console.Write("\nIngrese una opción: ");
+                    opcion = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                } while (opcion < 0 || opcion >= 5);
+                Console.Write("Ingrese x: ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese Y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 0: Console.WriteLine("Cerrando sistema"); return;
+                    case 1: suma(x, y); break;
+                    case 2: resta(x, y); break;
+                    case 3: multi(x, y); break;
+                    case 4: divi(x, y); break;
+                }
+
+                Console.Write("\n¿Desea regresar al menú? (S/N): ");
+                continuar = char.Parse(Console.ReadLine().ToLower());
                 Console.Clear();
-
-            } while (opcion < 0 || opcion >= 5);
+            } while (continuar == 's');
         }
 
-        static void suma(int x, int y)
-        {
-
-
+        static void suma(int x, int y) {
+            Console.WriteLine("La suma es: " + (x + y));
         }
 
-        static void resta(int x, int y)
-        {
-
-
+        static void resta(int x, int y) {
+            Console.WriteLine("La resta es: " + (x - y));
         }
 
-        static void multi(int x, int y)
-        {
-
-
+        static void multi(int x, int y) {
+            Console.WriteLine("La multiplicación es: " + (x * y));
         }
-
-        static void divi(int x, int y)
-        {
-
-
+        static void divi(int x, int y) {
+            if (y != 0)
+                Console.WriteLine("La división es: " + (x / y));
+            else Console.WriteLine("No se puede dividir entre 0");
         }
     }
 }
