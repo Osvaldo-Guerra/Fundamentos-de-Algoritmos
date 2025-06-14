447e02a43e7035a26d70dc4fd747ff6ee61bf737
Osvaldo <osvaldoguerra9909@gmail.com>
2025-06-14 10:15:53 -0500

    Ejercicios - Semana 12

---
+++ Semana11/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            int[] num = new int[5];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"Ingrese el número {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nNúmeros ingresados: ");
+            foreach (int n in num)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static void ejer2()
+        {
+            int[] num = { -2, 5, -1, 0, 8, -6, 3 };
+
+            Console.WriteLine("Arreglo original: ");
+            foreach (int n in num)
+                Console.Write(n + " ");
+
+            Console.WriteLine("\nArreglo modificado: ");
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] < 0)
+                    num[i] = 0;
+            }
+            foreach (int n in num)
+                Console.Write(n + " ");
+        }
+
+        static void ejer3()
+        {
+            int[] num = { 10, 50, 6, 3, 7, 20 };
+
+            Console.Write("num = [");
+            foreach (int n in num) ;
+            Console.Write(num + ", ");
+            Console.Write("]");
+
+            int minimo = num[0];
+            int maximo = num[0];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] < minimo)
+                    minimo = num[i];
+
+            }
+        }
+
+        static void ejer4()
+        {
+            int[] num = new int[10];
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"Ingrese número {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Números pares ingresados:");
+
+            foreach (int n in num)
+            {
+                if (n % 2 == 0)
+                    Console.WriteLine(n);
+            }
+        }
+
+
+        static void ejer5()
+        {
+            Console.Write("¿Cuántos números desea ingresar?: ");
+            int cant = int.Parse(Console.ReadLine());
+
+            int[] num = new int[cant];
+            int suma = 0;
+            Console.WriteLine();
+            for (int i = 0; i < cant; i++)
+            {
+                Console.Write($"Ingrese número {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+                suma += num[i];
+            }
+
+            Console.WriteLine("\nNúmeros ingresados: ");
+            foreach (int n in num)
+                Console.Write(n + " ");
+
+
+        }
+
+    }
+}
---
+++ Semana12/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana12
+{
+    class Program
+    {
+        static byte[] edades = new byte[0];
+        static int cantidad=0;
+        static void Main(string[] args)
+        {
+            int opcion;
+            string continuar;
+
+            do {
+                Console.WriteLine("1. Insertar");
+                Console.WriteLine("2. Mostrar");
+                Console.WriteLine("3. Eliminar");
+                Console.WriteLine("4. Ordenar");
+                Console.WriteLine("5. Salir");
+
+                Console.WriteLine("\nIngrese una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion) {
+                    case 1: insertar(); break;
+                    case 2: mostrar(); break;
+                    case 3: eliminar(); break;
+                    case 4: ordenar(); break;
+                    case 5: return;
+                }
+                Console.Write("Para continuar escriba (si): ");
+                continuar = Console.ReadLine();
+                Console.Clear();
+            } while (continuar == "si");
+        }
+
+        static void insertar() {
+            string cont;
+            do {
+                Console.Write("Ingrese la edad: ");
+                byte ed = byte.Parse(Console.ReadLine());
+
+                Array.Resize(ref edades, edades.Length + 1);
+                edades[cantidad] = ed;
+                cantidad++;
+
+                Console.WriteLine("\nRegistro correcto! \n\nDesea seguir registrando (si)?: ");
+                cont = Console.ReadLine();
+            } while (cont == "si");
+        }
+
+        static void mostrar() {
+            Console.WriteLine("\nEdades registrados: ");
+            for (int i = 0; i < edades.Length; i++) {
+                Console.WriteLine(edades[i]);
+            }
+        }
+
+        static void eliminar()
+        {
+            Console.WriteLine("\nIngrese el número a eliminar: ");
+            byte eli = byte.Parse(Console.ReadLine());
+            int indice = -1;
+
+            for (int i = 0; i < edades.Length; i++) {
+                if (edades[i] == eli) {
+                    indice = i;
+                }
+            }
+
+            if (indice != -1) {
+                for (int i = indice; i < edades.Length - 1; i++) {
+                    edades[i] = edades[i + 1];
+                }
+                Array.Resize(ref edades, edades.Length - 1);
+                cantidad--;
+                Console.WriteLine("\nEdad eliminado con exito!");
+            } else {
+                Console.WriteLine("\nEl número no existe no se puede eliminar!");
+            }
+        }
+
+        static void ordenar() {
+            for (int i = 0; i < edades.Length; i++) {
+                for (int j = 0; j < edades.Length - i - 1; j++) {
+                    if (edades[j] > edades[j + 1]) {
+                        byte temp = edades[i];
+                        edades[j] = edades[j + i];
+                        edades[j + i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nSe ordeno correctamente!");
+        }
+    }
+}
